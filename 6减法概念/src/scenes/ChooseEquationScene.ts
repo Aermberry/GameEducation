4799c40cedbadcd6b7a4796e9f98376eaac0e2f7
mobile.main <mobile.main@kanhan.com>
2018-08-14 01:50:31 +0800

    修改字体

--- 6减法概念/src/scenes/ChooseEquationScene.ts
+++ 6减法概念/src/scenes/ChooseEquationScene.ts
@@ -23,7 +23,7 @@ class ChooseEquationScene extends eui.Component implements  eui.UIComponent {
 		mouse.setButtonMode(this.math315Less123Rect, true);
 		mouse.setButtonMode(this.math321Less125Rect, true);
 		mouse.setButtonMode(this.math203Less24Rect, true);
-		this.math143Less21Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = '../../math143Less21/math143Less21.html', this);
+		this.math143Less21Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub1/index.html', this);
 	}
 	
 }
