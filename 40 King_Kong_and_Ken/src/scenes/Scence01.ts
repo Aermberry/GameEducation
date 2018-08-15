fe640fe756c4cb93ee1d4b00701da6680f9f6a94
Salmonberry <sos2012happy@gmail.com>
2018-08-15 17:18:45 +0800

    add resource

---
+++ 40 King_Kong_and_Ken/src/scenes/Scence01.ts
@@ -0,0 +1,27 @@
+class Scence01 extends eui.Component implements  eui.UIComponent {
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+
+		let data=RES.getRes("HittheGong.json");
+		let textr=RES.getRes("HittheGong.png");
+		let mcFactory:egret.MovieClipDataFactory= new egret.MovieClipDataFactory(data,textr);
+		let mc1:egret.MovieClip=new egret.MovieClip(mcFactory.generateMovieClipData("mc1"));
+
+		this.addChild(mc1);
+		mc1.gotoAndPlay(1,-1);
+	}
+
+
+	
+}
