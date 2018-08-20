668431b3c3575fc6db4b4a3da0a974931305b15a
Salmonberry <sos2012happy@gmail.com>
2018-08-20 10:16:21 +0800

    sd

---
+++ 40 King_Kong_and_Ken/libs/modules/lzlib/lzlib.d.ts
@@ -0,0 +1,97 @@
+interface Array<T> {
+    shuffle(): void;
+}
+declare namespace lzlib {
+    /**
+     * 使用方法
+     *  let drag = new Drag();
+        this.stage.addChild(drag);
+        drag.enableDrag(dragObject, true, data);
+    */
+    class Drag extends egret.Sprite {
+        static isDraging: boolean;
+        static dragingObject: egret.DisplayObject;
+        static isAccepted: boolean;
+        static dataTransfer: any;
+        static isCopy: boolean;
+        static originalParent: egret.DisplayObjectContainer;
+        private static originalX;
+        private static originalY;
+        private dragObject;
+        private isCopy;
+        private dataTransfer;
+        private distance;
+        constructor();
+        private onAddToStage(event);
+        enableDrag(dragObject: egret.DisplayObject, isCopy?: boolean, dataTransfer?: any): void;
+        private onTouchBegin(e);
+        private cloneDragObject(dragObject);
+        private onTouchMove(e);
+        private onTouchEnd(e);
+        private static init(dragingObject, isCopy, dataTransfer);
+        private static reset();
+        disableDrag(): void;
+    }
+}
+declare namespace lzlib {
+    class LzDragEvent extends egret.TouchEvent {
+        static readonly DRAG_OVER: string;
+        static readonly DRAG_OUT: string;
+        static readonly DROP: string;
+        dragObject: egret.DisplayObject;
+        constructor(type: string, dragObject: egret.DisplayObject, data: any, stageX: number, stageY: number, touchPointID?: number);
+    }
+}
+declare namespace lzlib {
+    /**
+     * 使用方法
+     *  let drop = new Drop();
+        this.stage.addChild(drop);
+        drop.enableDrag(dropObject);
+        dropObject.addEventListener(lzlib.LzDragEvent.DROP, this.onDrop, this);
+
+        onDrop(e: lzlib.DragEvent): void
+        {
+            //处理 e.dragData
+            //调用e.preventDefault()，表示dropObject接受拖入的对象
+            e.preventDefault();
+        }
+    */
+    class Drop extends egret.Sprite {
+        private dropObject;
+        constructor();
+        enableDrop(dropObject: egret.DisplayObject): void;
+        disableDrop(): void;
+        private onTouchEnd(e);
+    }
+}
+interface Math {
+    randomMinMax(min: number, max: number): number;
+}
+declare namespace egret {
+    interface MovieClip {
+        playAsync(): Promise<void>;
+    }
+}
+interface String {
+    replaceAt(index: number, replacement: string): string;
+}
+declare namespace lzlib {
+    class ThreadUtility {
+        static sleep(ms?: number): Promise<{}>;
+        static playSound(soundName: string): Promise<void>;
+    }
+}
+declare namespace egret.tween {
+    interface TweenGroup {
+        playOnceAsync(): Promise<void>;
+    }
+}
+declare namespace lzlib {
+    class TweenGroupUtility {
+        /**
+         * 循环播放动画
+         */
+        static playAnimation(target: egret.tween.TweenGroup): void;
+    }
+}
--- 40 King_Kong_and_Ken/src/scenes/Scene01.ts
+++ 40 King_Kong_and_Ken/src/scenes/Scene01.ts
@@ -13,6 +13,10 @@ class Scene01 extends eui.Component implements eui.UIComponent {
 	protected childrenCreated(): void {
 		super.childrenCreated();
 		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onNextButtonClick, this);
+		(RES.getRes('01a_mp3') as egret.Sound).play(0, 1);
+	}
+
+	private async playBackgroundMusic(): Promise<void> {
 	}
 
 	private onNextButtonClick(): void {
