892900a0ae2c3c9953838b487865c6c735de5528
chxu <chxu@liangzhicn.com>
2018-08-23 15:46:37 +0800

    no message

---
+++ 33 海龍公園一日遊/src/components/BackgroundComponent.ts
@@ -0,0 +1,34 @@
+class BackgroundComponent extends eui.Component implements  eui.UIComponent {
+
+	public titleText: eui.Label;
+	
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+		this.titleText.text = this._text;
+
+	}
+
+	private _text = '';
+
+	public get text(): string
+	{
+		return this._text;
+	}
+
+	public set text(val: string)
+	{
+		this._text = val;
+	}
+	
+}
---
+++ 33 海龍公園一日遊/src/scenes/NextPageScene.ts
@@ -0,0 +1,44 @@
+class NextPageScene extends eui.Component implements  eui.UIComponent {
+	
+	public curStep: eui.Label;
+	public nextPageButton: eui.Button;
+	
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+		this.curStep.text = this._text;
+
+		this.nextPageButton.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
+			this.onNextPage(this.curStep.text);
+		}, this)
+	}
+
+	private onNextPage(step: string)
+	{
+		console.log(step);
+		this.removeChildren();
+		this.addChild(new GameScene());
+	}
+
+	private _text = '';
+
+	public get text():string
+	{
+		return this._text;
+	}
+	
+	public set text(step: string)
+	{
+		this._text = step;
+	}
+}
