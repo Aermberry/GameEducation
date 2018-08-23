fa3e91fd77f1b2f536c96923ba35a475fbf8daa1
Salmonberry <sos2012happy@gmail.com>
2018-08-23 18:02:06 +0800

    Update:延遲動畫

--- 41 Packing Up - Put the Toys Back/src/Scenes/startScene.ts
+++ 41 Packing Up - Put the Toys Back/src/Scenes/startScene.ts
@@ -1,29 +1,27 @@
-class startScene extends eui.Component implements  eui.UIComponent {
-	private starBtn:eui.Button;
+class startScene extends eui.Component implements eui.UIComponent {
+	private startButton: eui.Button;
 	public constructor() {
 		super();
 	}
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
-		this.starBtn.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onnextBtnClick,this);
-		this.playBackgroundMusic();
+		this.startButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onnextBtnClick, this);
+		this.startGame();
 	}
 
-// 播放背景音樂
-	private async playBackgroundMusic():Promise<void> {
+	private async startGame(): Promise<void> {
 		await lzlib.SoundUtility.playSound('01a_mp3');
+		this.startButton.visible = true;
 	}
 
-// 點擊進入游戲場景
-	private onnextBtnClick():void {
+	// 點擊進入游戲場景
+	private onnextBtnClick(): void {
 		Main.instance.gotoScene(new gameScene());
 	}
 }
