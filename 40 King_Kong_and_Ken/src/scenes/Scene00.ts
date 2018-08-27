5717cec7a08baae4d416eed4f60d1d0febafafdf
Salmonberry <sos2012happy@gmail.com>
2018-08-27 11:06:37 +0800

    zz

--- 40 King_Kong_and_Ken/src/scenes/Scene00.ts
+++ 40 King_Kong_and_Ken/src/scenes/Scene00.ts
@@ -10,18 +10,17 @@ class Scene00 extends eui.Component implements eui.UIComponent {
 
 	protected childrenCreated(): void {
 		super.childrenCreated();
-		this.addEventListener(egret.TouchEvent.TOUCH_TAP, 
-		this.onSceneClick, this);
+		this.addEventListener(egret.TouchEvent.TOUCH_TAP,
+			this.onSceneClick, this);
 		this.playBackgroundMusic();
 	}
 
-	private async playBackgroundMusic():Promise<void> {
+	private async playBackgroundMusic(): Promise<void> {
 		await lzlib.SoundUtility.playSound('01a_mp3');
 		Main.instance.gotoScene(new Scene00_1());
 	}
 
-	private onSceneClick(): void 
-	{
+	private onSceneClick(): void {
 		Main.instance.gotoScene(new Scene00_1());
 	}
 }
