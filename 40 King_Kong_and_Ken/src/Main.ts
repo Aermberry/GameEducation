91f6588c1e197c6d91b1dd5bb0ab1921c815f542
Salmonberry <sos2012happy@gmail.com>
2018-08-31 18:56:25 +0800

    。。。。

--- 40 King_Kong_and_Ken/src/Main.ts
+++ 40 King_Kong_and_Ken/src/Main.ts
@@ -100,7 +100,7 @@ class Main extends eui.UILayer {
      */
     protected createGameScene(): void {
         Main.instance = this;
-        this.addChild(new Scene21())
+        this.addChild(new Scene11())
     }
 
     public static instance: Main
--- 40 King_Kong_and_Ken/src/scenes/Scene11.ts
+++ 40 King_Kong_and_Ken/src/scenes/Scene11.ts
@@ -1,5 +1,11 @@
 class Scene11 extends eui.Component implements eui.UIComponent {
 	private nextButton: eui.Button;
+	private display:egret.tween.TweenGroup;
+	private kenLabel:eui.Label;
+	private KingLabel:eui.Label;
+	private KongLabel:eui.Label;
+	private KingLabel0:eui.Label;
+
 	public constructor() {
 		super();
 	}
@@ -12,13 +18,25 @@ class Scene11 extends eui.Component implements eui.UIComponent {
 	protected childrenCreated(): void {
 		super.childrenCreated();
 		// this.onPlayBlackgroundMusic();
-		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onNextScene, this);
-		Base.onAsyncPlayBlackgroundMusic('27a_mp3', this.nextButton);
+		// this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onNextScene, this);
+		Base.onAsyncPlayBlackgroundMusic('27a_mp3');
+		this.display.play(0);
+		Base.onClickNextScene(this.nextButton,new Scene12())
 		// this.nextButton.visible=true;
 	}
 
 	private onNextScene(): void {
 		Main.instance.gotoScene(new Scene12());
+	}
+
+	private async changColor(element:eui.Label ):Promise<void> {
+		// var shap:egret.Shape
+		await lzlib.ThreadUtility.sleep(2000);
+		 element.textColor= 0xd92e20;
+		 await lzlib.ThreadUtility.sleep(2000);
+		 await lzlib.ThreadUtility.sleep(2000);
+		 await lzlib.ThreadUtility.sleep(2000);
+		 await lzlib.ThreadUtility.sleep(2000);
 	}
 
 	// private onPlayBlackgroundMusic():void{
