5422284b65209b0d5330c6715e0c942868ef9b42
Salmonberry <sos2012happy@gmail.com>
2018-09-05 14:53:12 +0800

    add BG voice&clickButton function for the StartScene

--- 44 Student Cards 1/src/scenes/StartScene.ts
+++ 44 Student Cards 1/src/scenes/StartScene.ts
@@ -1,5 +1,6 @@
 class StartScene extends eui.Component implements  eui.UIComponent {
 	private titleLabel:eui.Label;
+	private nextButton:eui.Button;
 	public constructor() {
 		super();
 	}
@@ -13,9 +14,16 @@ class StartScene extends eui.Component implements  eui.UIComponent {
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
+		this.onLoadVoice();
 		let underLine=new Base();
 		underLine.drawLine(this.titleLabel,this);
-
+		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onNextScene,this);
+	}
+	private onLoadVoice():void {
+		lzlib.SoundUtility.playSound('01_mp3');
+	}
+	private onNextScene():void{
+		Main.instance.gotoScene(new MainScene());
 	}
 	
 }
