bb0b9b69e39feb705fe2446e63611c3a026749d7
Salmonberry <sos2012happy@gmail.com>
2018-09-05 16:57:43 +0800

    add stopvoice

--- 44 Student Cards 1/src/scenes/StartScene.ts
+++ 44 Student Cards 1/src/scenes/StartScene.ts
@@ -1,6 +1,8 @@
 class StartScene extends eui.Component implements  eui.UIComponent {
 	private titleLabel:eui.Label;
 	private nextButton:eui.Button;
+	private sound:egret.Sound;
+	private soundChannel:egret.SoundChannel;
 	public constructor() {
 		super();
 	}
@@ -14,16 +16,26 @@ class StartScene extends eui.Component implements  eui.UIComponent {
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
-		this.onLoadVoice();
 		let underLine=new Base();
-		underLine.drawLine(this.titleLabel,this);
+		underLine.drawLine(this.titleLabel, this);//画线
+		this.sound = RES.getRes("01_mp3");//加载音频
+		this.soundChannel= this.sound.play(0, 1);//播放音频
 		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onNextScene,this);
 	}
-	private onLoadVoice():void {
-		lzlib.SoundUtility.playSound('01_mp3');
-	}
+
 	private onNextScene():void{
 		Main.instance.gotoScene(new MainScene());
+		this.stopSound();
+	}
+
+//检测voice是否在播放，停止	
+	private stopSound() {
+		var channel:egret.SoundChannel=this.soundChannel;
+		if(channel){
+			channel.stop();
+			this.soundChannel=null;
+			return;
+		}
 	}
 	
 }
