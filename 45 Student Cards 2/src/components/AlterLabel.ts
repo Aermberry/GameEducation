26db8c231dd11edf39ac6d62073dc68729cb7cef
Salmonberry <sos2012happy@gmail.com>
2018-09-10 15:12:52 +0800

    envirment

---
+++ 45 Student Cards 2/src/components/AlterLabel.ts
@@ -0,0 +1,40 @@
+namespace Ui {
+
+	export class AlterLabel extends eui.Component implements eui.UIComponent {
+		private textLabel: eui.Label;
+		public constructor() {
+			super();
+		}
+
+		protected partAdded(partName: string, instance: any): void {
+			super.partAdded(partName, instance);
+			
+		}
+
+		protected childrenCreated(): void {
+			super.childrenCreated();
+
+			this.textLabel.text = this.text;
+			this.textLabel.size = this.fontSize
+		}
+
+		private _text: string
+		private _fontSize:number
+
+		public set text(value: string) {
+			this._text = value;
+		}
+
+		public  get text() {
+			return this._text;
+		}
+
+		public set fontSize(value: number) {
+			this._fontSize = value;
+		}
+
+		public get fontSize() {
+			return this._fontSize
+		}
+	}
+}
