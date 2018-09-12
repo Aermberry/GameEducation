42c06a70bc08f91df8d22e5bdd523de31af4ffaf
Salmonberry <sos2012happy@gmail.com>
2018-09-12 14:07:35 +0800

    ok

--- 47 Card Writing 2 Sending Miss Chan a Card/src/scenes/MainScene.ts
+++ 47 Card Writing 2 Sending Miss Chan a Card/src/scenes/MainScene.ts
@@ -1,4 +1,9 @@
 class MainScene extends eui.Component implements  eui.UIComponent {
+	private dragGroup:eui.Group;
+	private dropGroup:eui.Group;
+	private tipsGroup:eui.Group;
+
+	private currentQuestionIndex=0;//问题的Id
 	public constructor() {
 		super();
 	}
@@ -8,10 +13,61 @@ class MainScene extends eui.Component implements  eui.UIComponent {
 		super.partAdded(partName,instance);
 	}
 
-
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
+		this.initDragable();
+	}
+
+	// 拖拽模块
+	private initDragable():void {
+		for(let child of this.dragGroup.$children) {
+			let drag=new lzlib.Drag();
+			this.stage.addChild(drag);
+			drag.enableDrag(child,false);
+		}
+		this.initDropableLabel();
+	}
+
+	private initDropableLabel():void {
+		let drop=new lzlib.Drop();
+		this.addChild(drop);
+		let child=this.dropGroup.getChildAt(this.currentQuestionIndex);
+		drop.enableDrop(child);
+		child.addEventListener(lzlib.LzDragEvent.DROP,this.onLabelDrop,this);
+	}
+
+	private onLabelDrop(e:lzlib.LzDragEvent):void {
+		let targetComponent=e.target as eui.Label;
+		let dragComponent=e.dragObject as eui.Label;
+
+		if(dragComponent.text.trim()==targetComponent.text.trim()){
+			e.preventDefault;
+			targetComponent.visible=true;
+			dragComponent.visible=false;
+
+			if(this.dropGroup.$children.every(child=>child.visible)){
+				lzlib.ThreadUtility.sleep(3000);
+				Main.instance.gotoScene(new FinishScene());
+			}
+			else{
+					this.currentQuestionIndex++;
+					this.initDragable();
+			}
+		}
+		else{
+			this.showTipsLabel();
+		}
 	}
 	
+		// tips模块
+	private async showTipsLabel():Promise<void>{
+		let originalChildIndex=this.getChildIndex(this.tipsGroup);//获取tipsgroup的Id
+		this.setChildIndex(this.tipsGroup,this.numChildren-1);//将yipsGroup层级升至最高
+		this.tipsGroup.getChildAt(this.currentQuestionIndex).visible=true;
+		await lzlib.ThreadUtility.sleep(2000);
+		this.tipsGroup.getChildAt(this.currentQuestionIndex).visible=false
+		this.setChildIndex(this.tipsGroup,originalChildIndex);//返回至原来的层级
+	}
+
 }
