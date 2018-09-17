537fba895e0f8265982b13aa2d95fe54d2a2d723
wghuang <wghuang@liangzhicn.com>
2018-09-17 10:23:17 +0800

    no message

---
+++ 12 减法应用练习题/src/components/EditableSmallLabel.ts
@@ -0,0 +1,19 @@
+class EditableSmallLabel extends EditableLabel implements  eui.UIComponent {
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+		// this.resultLabel.font = 'calculation_small_font_fnt';
+		this.userInput.textDisplay.size = 45;
+	}
+	
+}
