72efdbd8699402860c38c1fe14e0e22d40bde6e9
Salmonberry <sos2012happy@gmail.com>
2018-09-17 11:37:15 +0800

    scenes

--- 18 加減貨幣應用題練習/src/components/Background.ts
+++ 18 加減貨幣應用題練習/src/components/Background.ts
@@ -1,35 +1,32 @@
-namespace Ui{
-	export class Background extends eui.Component implements  eui.UIComponent {
-	private indexLabel:Ui.IndexTitle
-	public constructor() {
-		super();
-		this.skinName="BackgroundSkin";
+namespace Ui {
+	export class Background extends eui.Component implements eui.UIComponent {
+		private indexLabel: Ui.IndexTitle
+		public constructor() {
+			super();
+			this.skinName = "BackgroundSkin";
+		}
+
+		protected partAdded(partName: string, instance: any): void {
+			super.partAdded(partName, instance);
+		}
+
+
+		protected childrenCreated(): void {
+			super.childrenCreated();
+			
+			this.indexLabel.index = this.index;
+			console.log(this.indexLabel.index);
+		}
+
+		private _index: string;
+
+		public set index(value) {
+			this._index = value;
+			// this.indexLabel&&(this.indexLabel.index=value);
+		}
+
+		public get index(): string {
+			return this._index;
+		}
 	}
-
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
-	}
-
-
-	protected childrenCreated():void
-	{
-		super.childrenCreated();
-
-		this.indexLabel.index=this.index;
-		console.log(this.indexLabel.index);
-	}
-
-	private _index:string;
-
-	public set index(value){
-		this._index=value;
-		// this.indexLabel&&(this.indexLabel.index=value);
-	}
-
-	public get index():string{
-		return this._index;
-	}
-	
-}
 }
---
+++ 18 加減貨幣應用題練習/src/components/nextStepButton.ts
@@ -0,0 +1,31 @@
+class nextStepButton extends eui.Component implements  eui.UIComponent {
+
+	private nextStepButtonButton:eui.Image;
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+		mouse.enable(this.stage);
+		mouse.setButtonMode(this.nextStepButtonButton, true);
+		this.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onMouseOver, this);
+		this.addEventListener(mouse.MouseEvent.MOUSE_OUT,this.onMouseOut,this);
+	}
+
+	private onMouseOver(e: egret.TouchEvent): void {
+		this.currentState = "over";
+	}
+
+	private onMouseOut(e:egret.TouchEvent):void{
+		this.currentState='normal';
+	}
+	
+}
