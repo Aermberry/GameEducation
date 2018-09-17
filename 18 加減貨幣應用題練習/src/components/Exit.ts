ffb1b1427a79c29dfc9d52614bd0452755cd1e85
Salmonberry <sos2012happy@gmail.com>
2018-09-17 14:13:55 +0800

    fix problem about the components

--- 18 加減貨幣應用題練習/src/components/Exit.ts
+++ 18 加減貨幣應用題練習/src/components/Exit.ts
@@ -15,8 +15,10 @@ class Exit extends eui.Component implements  eui.UIComponent {
 		mouse.enable(this.stage);
 		mouse.setButtonMode(this.exitImage,true);
 		this.exitImage.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onExitImageTap,this);
-		this.exitImage.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onMouseOver, this);
-		this.exitImage.addEventListener(mouse.MouseEvent.MOUSE_OUT, this.onMouseOut, this);
+		// this.exitImage.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onMouseOver, this);
+		// this.exitImage.addEventListener(mouse.MouseEvent.MOUSE_OUT, this.onMouseOut, this);
+		this.addEventListener(mouse.MouseEvent.MOUSE_OVER,this.onMouseOver,this);
+		this.addEventListener(mouse.MouseEvent.MOUSE_OUT,this.onMouseOut,this);
 	}
 
 	private onExitImageTap(): void
@@ -26,11 +28,14 @@ class Exit extends eui.Component implements  eui.UIComponent {
 
 	private onMouseOver(e: egret.TouchEvent): void
 	{
-		(e.target as eui.Image).source = 'exit_over_png'
+		// (e.target as eui.Image).source = 'exit_over_png'
+		this.currentState="over";
+
 	}
 
 	private onMouseOut(e:egret.TouchEvent):void {
-		(e.target as eui.Image).source = 'exit_png';
+		// (e.target as eui.Image).source = 'exit_png';
+		this.currentState="normal";
 	}
 	
 }
--- 18 加減貨幣應用題練習/src/components/NextQuestion.ts
+++ 18 加減貨幣應用題練習/src/components/NextQuestion.ts
@@ -1,29 +1,30 @@
-class NextQuestion extends eui.Component implements eui.UIComponent {
+namespace Ui {
+	export class NextQuestion extends eui.Component implements eui.UIComponent {
+		private nextQuestionButton: eui.Image;
+		public constructor() {
+			super();
+		}
 
-	private NextQuestionButtom: eui.Image;
-	public constructor() {
-		super();
+		protected partAdded(partName: string, instance: any): void {
+			super.partAdded(partName, instance);
+		}
+
+
+		protected childrenCreated(): void {
+			super.childrenCreated();
+			mouse.enable(this.stage);
+			mouse.setButtonMode(this.nextQuestionButton, true);
+			this.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onMouseOver, this);
+			this.addEventListener(mouse.MouseEvent.MOUSE_OUT, this.onMouseOut, this);
+		}
+
+		private onMouseOver(e: egret.TouchEvent): void {
+			this.currentState = "over";
+		}
+
+		private onMouseOut(e: egret.TouchEvent): void {
+			this.currentState = 'normal';
+		}
+
 	}
-
-	protected partAdded(partName: string, instance: any): void {
-		super.partAdded(partName, instance);
-	}
-
-
-	protected childrenCreated(): void {
-		super.childrenCreated();
-		mouse.enable(this.stage);
-		mouse.setButtonMode(this.NextQuestionButtom, true);
-		this.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onMouseOver, this);
-		this.addEventListener(mouse.MouseEvent.MOUSE_OUT,this.onMouseOut,this);
-	}
-
-	private onMouseOver(e: egret.TouchEvent): void {
-		this.currentState = "over";
-	}
-
-	private onMouseOut(e:egret.TouchEvent):void{
-		this.currentState='normal';
-	}
-
 }
