470093c588734b7d9cbc4ab916ab915b6479ea23
Salmonberry <sos2012happy@gmail.com>
2018-09-19 16:57:54 +0800

    ...

---
+++ 18 加減貨幣應用題練習/src/components/InputExpressionPresenter.ts
@@ -0,0 +1,33 @@
+class InputExpressionPresenter {
+    private view: IInputExpressionView;
+    private expression = '';//用户输入模式
+
+    public constructor() {
+
+    }
+
+    public loadView(view: IInputExpressionView): void {
+        this.view = view;
+    }
+
+    public onNumberPadKeyDown(e: KeyDownEvent): void {
+        if (e.char == '' && this.expression.length > 0) {
+            this.expression = this.expression.substr(0, this.expression.length - 1);
+        }
+        else {
+            this.expression += e.char;
+        }
+        this.view.expression = this.expression;
+    }
+
+    public onSubmitButtonClick(): void {
+    //     if (this.expression.indexOf('+') >= 0) {
+    //         let operands = this.expression.split('+');
+    //         this.view.openAddCalculationScene(parseInt(operands[0], 10), parseInt(operands[1], 10)
+    //         );
+    //     } else {
+    //         let operands = this.expression.split('-');
+    //         this.view.openSubtractCalculationScene(parseInt(operands[0], 10), parseInt(operands[1], 10));
+    //     }
+    }
+}
--- 18 加減貨幣應用題練習/src/scenes/Question1Scene.ts
+++ 18 加減貨幣應用題練習/src/scenes/Question1Scene.ts
@@ -3,25 +3,68 @@ class Question1Scene extends eui.Component implements eui.UIComponent {
 	private titleLabel: eui.Label;
 	private ColorAnimation: egret.tween.TweenGroup;
 	private Beers: egret.tween.TweenGroup;
-	private worldsGroup:eui.Group;
-	private nextStepButton:ui.nextStepButton;
-	private nextQuestionButton:Ui.NextQuestion;
+	private worldsGroup: eui.Group;
+	private nextQuestionButton: Ui.NextQuestion;
+	private expressionLabel: eui.Label;
+	private numberPad: NumberPad;
+	private nextStepButton: ui.nextStepButton;
 
 	public constructor() {
 		super();
 	}
 
 	protected partAdded(partName: string, instance: any): void {
+
 		super.partAdded(partName, instance);
 	}
 
 
 	protected childrenCreated(): void {
 		super.childrenCreated();
+		mouse.enable(this.stage);
+
 		this.playBackMusic();
 		this.DisplayAnimation();
-		this.nextQuestionButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onNextQuestionPage,this)
+
+		this.nextQuestionButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onNextQuestionPage, this)
+		this.numberPad.addEventListener(KeyDownEvent.EVENT, this.onNumberPadKeyDown, this);
+		this.nextStepButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.nextStep, this);
+		this.expressionLabel.text = this.expression;
 	}
+
+	private _expression: string;
+	public set expression(value: string) {
+		this._expression = value;
+	}
+
+	public get expression() {
+		return this._expression;
+	}
+
+	private onNumberPadKeyDown(e: KeyDownEvent): void {
+		if (e.char == '' && this.expression.length > 0) {
+			this._expression = this.expression.substr(0, this.expression.length - 1);
+		}
+		else {
+			this._expression += e.char;
+		}
+		console.log(this._expression);
+	}
+
+	private nextStep(): void {
+
+	}
+
+
+	public openAddCalculationScene(addend: number, augend: number): void {
+
+	}
+
+	public openSubtractCalculationScene(minuend: number, subtrahend: number): void {
+
+	}
+
+
 
 	private async DisplayAnimation(): Promise<void> {
 		await lzlib.ThreadUtility.sleep(500);
@@ -34,12 +77,13 @@ class Question1Scene extends eui.Component implements eui.UIComponent {
 		lzlib.ThreadUtility.sleep(1000);
 		await lzlib.SoundUtility.playSound("think_mp3");
 		await this.Beers.play(0);
-		setTimeout(()=>{this.worldsGroup.visible=true;},1000);
+		setTimeout(() => { this.worldsGroup.visible = true; }, 1000);
 		await lzlib.ThreadUtility.sleep(3000);
-		this.nextStepButton.visible=true;
+		this.nextStepButton.visible = true;
+		this.expressionLabel.visible = true;
 	}
 
-	private onNextQuestionPage():void {
+	private onNextQuestionPage(): void {
 		Base.gotoNextScene(new Question2Scene);
 	}
 }
