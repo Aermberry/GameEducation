4850471e7598396ca4efa192b07fd597cd4daf4a
Salmonberry <sos2012happy@gmail.com>
2018-09-25 09:25:53 +0800

    scene ui

--- 48 Card Writing 3 Tom's Halloween Party/src/scenes/StartScene.ts
+++ 48 Card Writing 3 Tom's Halloween Party/src/scenes/StartScene.ts
@@ -1,17 +1,21 @@
-class StartScene extends eui.Component implements  eui.UIComponent {
+class StartScene extends eui.Component implements eui.UIComponent {
+	private nextButton:eui.Button;
 	public constructor() {
 		super();
 	}
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
+		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.nextScene,this);
 	}
-	
+
+	private nextScene():void {
+		Main.instance.gotoScene(new MainScene());
+	}
+
 }
