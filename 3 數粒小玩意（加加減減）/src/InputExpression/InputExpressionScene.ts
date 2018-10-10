5461a81930927a61681bb48a6ae4071c560c5a24
chxu <chxu@liangzhicn.com>
2018-10-10 12:00:49 +0800

    修改

--- 3 數粒小玩意（加加減減）/src/InputExpression/InputExpressionScene.ts
+++ 3 數粒小玩意（加加減減）/src/InputExpression/InputExpressionScene.ts
@@ -3,6 +3,9 @@ class InputExpressionScene extends eui.Component implements  eui.UIComponent, II
 	private numberPad: NumberPad;
 	private expressionLabel: eui.Label;
 	private submitButton: ImageButton;
+	private startButton2: eui.Image;
+	private startMask: eui.Rect;
+	
 
 	private presenter = new InputExpressionPresenter();
 
@@ -24,7 +27,15 @@ class InputExpressionScene extends eui.Component implements  eui.UIComponent, II
 		this.exitButton.addEventListener(egret.TouchEvent.TOUCH_TAP, () => window.close(), this);
 		this.numberPad.addEventListener(KeyDownEvent.EVENT, this.presenter.onNumberPadKeyDown, this.presenter);
 		this.submitButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.presenter.onSubmitButtonClick, this.presenter);
+		this.startButton2.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStartButton2Click, this);
 
+		
+	}
+
+	private onStartButton2Click(): void
+	{
+		this.startMask.visible = false;
+		this.startButton2.visible = false;
 		this.presenter.loadView(this);
 	}
 
