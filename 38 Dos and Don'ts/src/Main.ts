9ee4dd80249f4507f4f6d68cff9cf27aabc3395e
Salmonberry <sos2012happy@gmail.com>
2018-10-12 09:18:28 +0800

    Fix:ENG_0050

--- 38 Dos and Don'ts/src/Main.ts
+++ 38 Dos and Don'ts/src/Main.ts
@@ -102,7 +102,7 @@ class Main extends eui.UILayer {
      */
     protected createGameScene(): void {
         Main.instance = this;
-        this.addChild(new BicycleScene8());
+        this.addChild(new LeaveAndEnterScene4());
     }
 
     public static instance: Main
