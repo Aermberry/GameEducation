420acaac10bbde7c6991f9fa665d1ade8728e9dd
Salmonberry <sos2012happy@gmail.com>
2018-10-12 11:52:23 +0800

    add Mask function

--- 53 Haunted House/src/scenes/StartScene.ts
+++ 53 Haunted House/src/scenes/StartScene.ts
@@ -1,31 +1,43 @@
-class StartScene extends eui.Component implements eui.UIComponent{
+class StartScene extends eui.Component implements eui.UIComponent {
 
-        private batTweenGroup:egret.tween.TweenGroup;
-        private bgmSoundChannel:egret.SoundChannel;
+    private batTweenGroup: egret.tween.TweenGroup;
+    private bgmSoundChannel: egret.SoundChannel;
+    private startMask: eui.Rect;
+    private startButton: eui.Button;
+    private loadingAnim: egret.tween.TweenGroup
 
-        public constructor(){
-            super();   
-        }
-        
-        protected partAdded(partName:string,instance:any):void
-        {
-            super.partAdded(partName, instance);
-        }
-        
 
-        protected childrenCreated():void
-        {   
-            super.childrenCreated();
-            this.bgmSoundChannel = RES.getRes('start_scene_bgm_mp3').play(0,1);
-            this.startScene();
-        }
+    public constructor() {
+        super();
+    }
 
-        private async startScene(): Promise<void>
-        {
-            TweenGroupUtility.playAnimation(this.batTweenGroup);
-            await ThreadUtility.sleep(2000);
-            this.batTweenGroup.stop();
-            this.bgmSoundChannel.stop();
-            Main.instance.gotoScene(new MoonRiseScene());
-        }
+    protected partAdded(partName: string, instance: any): void {
+        super.partAdded(partName, instance);
+    }
+
+
+    protected childrenCreated(): void {
+        super.childrenCreated();
+        this.startButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onStartButtonClick,this);
+    }
+
+    private async onStartButtonClick(): Promise<void> {
+        await this.loadingAnim.play(0);
+        this.startMask.visible = false;
+        this.startButton.visible = false;
+        this.loadMusic();
+        this.startScene();
+    }
+
+    private loadMusic(): void {
+        this.bgmSoundChannel = RES.getRes('start_scene_bgm_mp3').play(0, 1);
+    }
+
+    private async startScene(): Promise<void> {
+        TweenGroupUtility.playAnimation(this.batTweenGroup);
+        await ThreadUtility.sleep(2000);
+        this.batTweenGroup.stop();
+        this.bgmSoundChannel.stop();
+        Main.instance.gotoScene(new MoonRiseScene());
+    }
 }
