d99334d377dde06b29a62cec3cd18aac8d2f13f6
wghuang <wghuang@liangzhicn.com>
2018-10-14 21:04:18 +0800

    merge

--- 6 减法概念/src/scenes/ChooseEquationScene.ts
+++ 6 减法概念/src/scenes/ChooseEquationScene.ts
@@ -23,11 +23,15 @@ class ChooseEquationScene extends eui.Component implements  eui.UIComponent {
 		mouse.setButtonMode(this.math315Less123Rect, true);
 		mouse.setButtonMode(this.math321Less125Rect, true);
 		mouse.setButtonMode(this.math203Less24Rect, true);
-		this.math143Less21Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub1/index.html', this);
+		this.math143Less21Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onMath143Less21RectClick, this);
 		this.math134Less115Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub2/index.html', this);
 		//this.math315Less123Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub3/index.html', this);
 		//this.math321Less125Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub4/index.html', this);
 		//this.math203Less24Rect.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>window.location.href = 'Asub5/index.html', this);
 	}
 	
+	private onMath143Less21RectClick():void
+	{	
+		init();
+	}
 }
