e03aaeb16e24c7b2dacdadf85573162d07b9d95f
Salmonberry <sos2012happy@gmail.com>
2018-10-17 18:06:09 +0800

    add interface

---
+++ 52 Fun with Riddles 2/src/Game/GameView.ts
@@ -0,0 +1,15 @@
+interface GameView {
+
+  // 跳轉頁面
+  gotoNextScene(): void
+  // Drag功能
+  initDrag(): void
+  // Drop功能
+  initDropable(): void
+  // Drop配對功能
+  onLabelDrop(): void
+  // Drop不配對功能
+  onDragCancel(): void
+  // 顯示提示
+  showTIpslabel(): void
+}
--- 52 Fun with Riddles 2/src/scenes/Game01Scene.ts
+++ 52 Fun with Riddles 2/src/scenes/Game01Scene.ts
@@ -1,17 +1,40 @@
-class Game01Scene extends eui.Component implements  eui.UIComponent {
+class Game01Scene extends eui.Component implements eui.UIComponent, GameView {
 	public constructor() {
 		super();
 	}
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
 	}
-	
+
+	public gotoNextScene(scene: eui.Component): void {
+
+		Main.instance.gotoScene(scene);
+
+	}
+	// Drag功能
+	public initDrag(): void {
+		
+	}
+	// Drop功能
+	public initDropable(): void {
+
+	}
+	// Drop配對功能
+	public onLabelDrop(): void {
+
+	}
+	// Drop不配對功能
+	public onDragCancel(): void {
+
+	}
+	// 顯示提示
+	public showTIpslabel(): void {
+
+	}
 }
