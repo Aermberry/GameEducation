1538697b7419eb6497b61ca8cfc4e8bc909bd7e1
Salmonberry <sos2012happy@gmail.com>
2018-10-23 15:23:01 +0800

    add startButton

--- 50 Choosing Presents/src/scenes/StatueScene.ts
+++ 50 Choosing Presents/src/scenes/StatueScene.ts
@@ -1,23 +1,32 @@
-class StatueScene extends eui.Component implements  eui.UIComponent {
-	private nextButton:eui.Button;
+class StatueScene extends eui.Component implements eui.UIComponent {
+	private nextButton: eui.Button;
+	private startMask: eui.Rect;
+	private startButton: eui.Button;
+	private loadingAnim: egret.tween.TweenGroup
 	public constructor() {
 		super();
 	}
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
-		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP,this.gotoNextScene,this);
+		this.nextButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.gotoNextScene, this);
+		this.startButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStartButtonClick, this);
 	}
 
-	private gotoNextScene():void {
-		Main.instance.gotoScene(new Choose1Scene())		
+	private async onStartButtonClick(): Promise<void> {
+		await this.loadingAnim.play(0);
+		this.startMask.visible = false;
+		this.startButton.visible = false;
+		await lzlib.ThreadUtility.sleep(2000)
 	}
-	
+
+	private gotoNextScene(): void {
+		Main.instance.gotoScene(new Choose1Scene())
+	}
+
 }
