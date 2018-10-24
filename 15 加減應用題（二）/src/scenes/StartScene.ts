37ec49fcca563b8347932f02eb9cb847d16ece3a
chxu <chxu@liangzhicn.com>
2018-10-24 10:43:04 +0800

    bug 修改

--- 15 加減應用題（二）/src/scenes/StartScene.ts
+++ 15 加減應用題（二）/src/scenes/StartScene.ts
@@ -1,4 +1,8 @@
 class StartScene extends eui.Component implements  eui.UIComponent {
+
+	private startButton2: eui.Image;
+	private startMask: eui.Rect;
+
 	private splashTweenGroup: egret.tween.TweenGroup;
 
 	public constructor() {
@@ -10,13 +14,19 @@ class StartScene extends eui.Component implements  eui.UIComponent {
 		super.partAdded(partName,instance);
 	}
 
-
 	protected childrenCreated():void
 	{
 		super.childrenCreated();
 		mouse.enable(this.stage);
 		this.splashTweenGroup.addEventListener(egret.Event.COMPLETE, this.onSplashTweenGroupComplete, this);
+		this.startButton2.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStartButton2Click, this);
+	}
+
+	private onStartButton2Click(): void
+	{
 		this.splashTweenGroup.play(0);
+		this.startMask.visible = false;
+		this.startButton2.visible = false;
 	}
 
 	private onSplashTweenGroupComplete(e: egret.Event): void
