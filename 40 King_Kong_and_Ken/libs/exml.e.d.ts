b524cdc38c00e77af514258c881a3d94dcbbb6e9
wghuang <wghuang@liangzhicn.com>
2018-10-25 14:19:46 +0800

    wghuang

--- 40 King_Kong_and_Ken/libs/exml.e.d.ts
+++ 40 King_Kong_and_Ken/libs/exml.e.d.ts
@@ -2,6 +2,56 @@ declare class btn_nextSkin extends eui.Skin{
 }
 declare class MovieClipPlayerSkin extends eui.Skin{
 }
+declare class Scene00Skin extends eui.Skin{
+}
+declare class Scene01Skin extends eui.Skin{
+}
+declare class Scene02Skin extends eui.Skin{
+}
+declare class Scene03Skin extends eui.Skin{
+}
+declare class Scene04Skin extends eui.Skin{
+}
+declare class Scene05Skin extends eui.Skin{
+}
+declare class Scene06Skin extends eui.Skin{
+}
+declare class Scene07Skin extends eui.Skin{
+}
+declare class Scene08Skin extends eui.Skin{
+}
+declare class Scene09Skin extends eui.Skin{
+}
+declare class Scene10Skin extends eui.Skin{
+}
+declare class Scene11Skin extends eui.Skin{
+}
+declare class Scene12Skin extends eui.Skin{
+}
+declare class Scene13Skin extends eui.Skin{
+}
+declare class Scene14Skin extends eui.Skin{
+}
+declare class Scene15Skin extends eui.Skin{
+}
+declare class Scene16Skin extends eui.Skin{
+}
+declare class Scene17Skin extends eui.Skin{
+}
+declare class Scene18Skin extends eui.Skin{
+}
+declare class Scene19Skin extends eui.Skin{
+}
+declare class Scene20Skin extends eui.Skin{
+}
+declare class Scene21Skin extends eui.Skin{
+}
+declare class Scene22Skin extends eui.Skin{
+}
+declare class Scene23Skin extends eui.Skin{
+}
+declare class Scene24Skin extends eui.Skin{
+}
 declare module skins{
 	class ButtonSkin extends eui.Skin{
 	}
@@ -55,54 +105,4 @@ declare module skins{
 declare module skins{
 	class VSliderSkin extends eui.Skin{
 	}
-}
-declare class Scene00Skin extends eui.Skin{
-}
-declare class Scene01Skin extends eui.Skin{
-}
-declare class Scene02Skin extends eui.Skin{
-}
-declare class Scene03Skin extends eui.Skin{
-}
-declare class Scene04Skin extends eui.Skin{
-}
-declare class Scene05Skin extends eui.Skin{
-}
-declare class Scene06Skin extends eui.Skin{
-}
-declare class Scene07Skin extends eui.Skin{
-}
-declare class Scene08Skin extends eui.Skin{
-}
-declare class Scene09Skin extends eui.Skin{
-}
-declare class Scene10Skin extends eui.Skin{
-}
-declare class Scene11Skin extends eui.Skin{
-}
-declare class Scene12Skin extends eui.Skin{
-}
-declare class Scene13Skin extends eui.Skin{
-}
-declare class Scene14Skin extends eui.Skin{
-}
-declare class Scene15Skin extends eui.Skin{
-}
-declare class Scene16Skin extends eui.Skin{
-}
-declare class Scene17Skin extends eui.Skin{
-}
-declare class Scene18Skin extends eui.Skin{
-}
-declare class Scene19Skin extends eui.Skin{
-}
-declare class Scene20Skin extends eui.Skin{
-}
-declare class Scene21Skin extends eui.Skin{
-}
-declare class Scene22Skin extends eui.Skin{
-}
-declare class Scene23Skin extends eui.Skin{
-}
-declare class Scene24Skin extends eui.Skin{
 }
