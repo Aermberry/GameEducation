4c19832447b94c20de77332bdd06057adf70a8b5
Salmonberry <sos2012happy@gmail.com>
2018-10-31 11:29:30 +0800

    startscene

--- 41 Packing Up - Put the Toys Back/src/Main.ts
+++ 41 Packing Up - Put the Toys Back/src/Main.ts
@@ -102,7 +102,7 @@ class Main extends eui.UILayer {
      */
     protected createGameScene(): void {
         Main.instance = this;
-        this.addChild(new gameScene());
+        this.addChild(new startScene());
     }
 
     public static instance: Main
