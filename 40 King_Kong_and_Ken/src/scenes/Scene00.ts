3e98d63b8b3ab35d013af928e41d2f87e6854d27
Salmonberry <sos2012happy@gmail.com>
2018-11-01 11:10:05 +0800

    amend the word

--- 40 King_Kong_and_Ken/src/scenes/Scene00.ts
+++ 40 King_Kong_and_Ken/src/scenes/Scene00.ts
@@ -35,7 +35,7 @@ class Scene00 extends eui.Component implements eui.UIComponent {
 
 	private async playBackgroundMusic(): Promise<void> {
 		await lzlib.SoundUtility.playSound('01a_mp3');//加載音頻
-		this.textLabel.text = "Let's listen to  the poem about them.";//文字變換
+		this.textLabel.text = "Let's listen to a poem about them.";//文字變換
 		this.textLabel.textColor = 0x5d20f5;//我腦子顔色變換
 		await lzlib.SoundUtility.playSound('01b_mp3')
 		// await lzlib.ThreadUtility.sleep(3000);
