45f8091e271968996fc32c4e63c0910cb0f5a170
chxu <chxu@liangzhicn.com>
2018-11-06 17:10:20 +0800

    脚本

--- 42 Saving Princess Nancy/src/scenes/SelectTaskScene.ts
+++ 42 Saving Princess Nancy/src/scenes/SelectTaskScene.ts
@@ -1,4 +1,12 @@
 class SelectTaskScene extends eui.Component implements  eui.UIComponent {
+	
+	private task1Label: eui.Label;
+	private task2Label: eui.Label;
+	private maskRect: eui.Rect;
+
+	private maskAnimation: egret.tween.TweenGroup;
+	private finishMaskAnimation: egret.tween.TweenGroup;
+	
 	public constructor() {
 		super();
 	}
@@ -9,9 +17,89 @@ class SelectTaskScene extends eui.Component implements  eui.UIComponent {
 	}
 
 
-	protected childrenCreated():void
+	protected async childrenCreated(): Promise<void>
 	{
 		super.childrenCreated();
+		mouse.enable(this.stage);
+		mouse.setButtonMode(this.task1Label, true);
+		mouse.setButtonMode(this.task2Label, true);
+		lzlib.SoundUtility.playSound('select_task_mp3');
+		await this.playMaskAnimation();
+		this.maskRect.visible = false;
+		this.task1Label.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onTask1Over, this);
+		this.task2Label.addEventListener(mouse.MouseEvent.MOUSE_OVER, this.onTask2Over, this);
+		this.task1Label.addEventListener(mouse.MouseEvent.MOUSE_OUT, this.onTask1Out, this);
+		this.task2Label.addEventListener(mouse.MouseEvent.MOUSE_OUT, this.onTask2Out, this);
+		this.task1Label.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onTask1Begin, this);
+		this.task2Label.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onTask2Begin, this);
+		this.task1Label.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTask1Click, this);
+		this.task2Label.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTask2Click, this);
+		this.task1Label.addEventListener(egret.TouchEvent.TOUCH_END, this.onTask1End, this);
+		this.task2Label.addEventListener(egret.TouchEvent.TOUCH_END, this.onTask2End, this);
+	}
+
+	private onTask1Begin(): void
+	{
+		this.task1Label.textColor = 0xf33b3d;
+	}
+
+	private onTask2Begin(): void
+	{
+		this.task2Label.textColor = 0xf33b3d;
+	}
+
+	private onTask1Over(): void
+	{
+		this.task1Label.textColor = 0xc38f44;
+	}
+
+	private onTask2Over(): void
+	{
+		this.task2Label.textColor = 0xc38f44;
+	}
+
+	private onTask1Out(): void
+	{
+		this.task1Label.textColor = 0x7E512F;
+	}
+
+	private onTask2Out(): void
+	{
+		this.task2Label.textColor = 0x7E512F;
+	}
+
+	private onTask1End(): void
+	{
+		this.task1Label.textColor = 0x7E512F;
+	}
+
+	private onTask2End(): void
+	{
+		this.task2Label.textColor = 0x7E512F;
+	}
+
+	private async onTask1Click(): Promise<void>
+	{
+		await this.playMaskAnimation(true);
+		Main.instance.gotoScene(new Task1IntroductionScene());
+	}
+
+	private async onTask2Click(): Promise<void>
+	{
+		await this.playMaskAnimation(true);
+		Main.instance.gotoScene(new Task1IntroductionScene());
+	}
+
+	private async playMaskAnimation(isFinish = false): Promise<void>
+	{
+		this.maskRect.visible = true;
+		if(isFinish){
+			await this.finishMaskAnimation.playOnceAsync();
+		}else{
+			await this.maskAnimation.playOnceAsync();
+		}
+		
+		this.maskRect.visible = false;
 	}
 	
 }
--- 42 Saving Princess Nancy/src/scenes/Task1IntroductionScene.ts
+++ 42 Saving Princess Nancy/src/scenes/Task1IntroductionScene.ts
@@ -1,4 +1,9 @@
 class Task1IntroductionScene extends eui.Component implements  eui.UIComponent {
+	
+	private maskRect: eui.Rect;
+	private nextBootsComponent: egret.tween.TweenGroup;
+	private maskAnimation: egret.tween.TweenGroup;
+	
 	public constructor() {
 		super();
 	}
@@ -9,9 +14,18 @@ class Task1IntroductionScene extends eui.Component implements  eui.UIComponent {
 	}
 
 
-	protected childrenCreated():void
+	protected async childrenCreated(): Promise<void>
 	{
 		super.childrenCreated();
+		lzlib.SoundUtility.playSound('task2_mp3');
+		await this.maskAnimation.playOnceAsync();
+		this.maskRect.visible = false;
+		this.nextBootsComponent.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onNextClick, this);
+	}
+
+	private onNextClick(e: egret.TouchEvent): void
+	{
+		Main.instance.gotoScene(new DragScene());
 	}
 	
 }
