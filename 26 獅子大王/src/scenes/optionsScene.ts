0ac6759b66e0018eb7f8bf6093a2904188a0485e
Salmonberry <sos2012happy@gmail.com>
2018-11-14 10:03:11 +0800

    add dialogBox Animation

--- 26 獅子大王/src/scenes/optionsScene.ts
+++ 26 獅子大王/src/scenes/optionsScene.ts
@@ -1,17 +1,20 @@
-class optionsScene extends eui.Component implements  eui.UIComponent {
-	public constructor() {
-		super();
-	}
+class optionsScene extends eui.Component implements eui.UIComponent {
+  private invitationAnimation:egret.tween.TweenGroup;
+  public constructor() {
+    super();
+  }
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
-	}
+  protected partAdded(partName: string, instance: any): void {
+    super.partAdded(partName, instance);
+  }
 
-
-	protected childrenCreated():void
-	{
+  protected childrenCreated(): void {
 		super.childrenCreated();
+		this.startLoadingAnimation();
 	}
 	
+	//開場動畫播放
+	private startLoadingAnimation():void{
+		this.invitationAnimation.play(0);
+	}
 }
