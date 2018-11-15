25ce462b0f38331aeead767a58d93bb27f07e30c
Salmonberry <sos2012happy@gmail.com>
2018-11-15 15:22:42 +0800

    add achieveComponent

--- 26 獅子大王/src/components/achieveComponent.ts
+++ 26 獅子大王/src/components/achieveComponent.ts
@@ -1,17 +1,36 @@
-class achieveComponent extends eui.Component implements  eui.UIComponent {
+class achieveComponent extends eui.Component implements eui.UIComponent {
+	private flexAnim: egret.tween.TweenGroup;
 	public constructor() {
 		super();
 	}
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
 	}
 
 
-	protected childrenCreated():void
-	{
+	protected childrenCreated(): void {
 		super.childrenCreated();
+		this.playAnimation(this.flexAnim, true);
 	}
-	
+
+	playAnimation(target: egret.tween.TweenGroup, isLoop: boolean): void {
+		if (isLoop) {
+			for (var key in target.items) {
+				target.items[key].props = { loop: true };
+			}
+		}
+		target.play();
+	}
+
+	private status(status: boolean = false): void {
+		status ? this.currentState = "normal" : this.currentState = "active"
+	}
+
+	private gotoHomeScene(): void {
+		let currentState = this.getCurrentState()
+		if (currentState == "active")
+			Main.instance.gotoScene(new optionsScene());
+	}
+
 }
