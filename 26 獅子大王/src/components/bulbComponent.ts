07bc620fc1308442f1f22e9a00112bae2cf55b49
Salmonberry <sos2012happy@gmail.com>
2018-11-15 17:37:39 +0800

    amend UI

--- 26 獅子大王/src/components/bulbComponent.ts
+++ 26 獅子大王/src/components/bulbComponent.ts
@@ -27,6 +27,10 @@ class bulbComponent extends eui.Component implements eui.UIComponent {
 		target.play();
 	}
 
+	private status(statue:string ):void {
+		this.currentState=statue;
+	}
+
 }
 
 
--- 26 獅子大王/src/scenes/rabbitScene.ts
+++ 26 獅子大王/src/scenes/rabbitScene.ts
@@ -1,17 +1,21 @@
-class rabbitScene extends eui.Component implements  eui.UIComponent {
-	public constructor() {
-		super();
-	}
+class rabbitScene extends eui.Component implements eui.UIComponent {
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
-	}
-
-
-	protected childrenCreated():void
-	{
-		super.childrenCreated();
-	}
+  private plantMask: egret.tween.TweenGroup;
+  private tailWiggle: egret.tween.TweenGroup;
+  private invitationCard: egret.tween.TweenGroup;
+  private lionDialog: egret.tween.TweenGroup;
+  private rabbitDialogBox: egret.tween.TweenGroup;
+	private bubleGrad: egret.tween.TweenGroup;
 	
+  public constructor() {
+    super();
+  }
+
+  protected partAdded(partName: string, instance: any): void {
+    super.partAdded(partName, instance);
+  }
+
+  protected childrenCreated(): void {
+    super.childrenCreated();
+  }
 }
