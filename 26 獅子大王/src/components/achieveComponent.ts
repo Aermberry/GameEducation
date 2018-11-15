e673e327184d522e2126d5a870b2692a1a7faf73
Salmonberry <sos2012happy@gmail.com>
2018-11-15 16:52:22 +0800

    add bulb components

--- 26 獅子大王/src/components/achieveComponent.ts
+++ 26 獅子大王/src/components/achieveComponent.ts
@@ -14,7 +14,7 @@ class achieveComponent extends eui.Component implements eui.UIComponent {
 		this.playAnimation(this.flexAnim, true);
 	}
 
-	playAnimation(target: egret.tween.TweenGroup, isLoop: boolean): void {
+	private playAnimation(target: egret.tween.TweenGroup, isLoop: boolean): void {
 		if (isLoop) {
 			for (var key in target.items) {
 				target.items[key].props = { loop: true };
---
+++ 26 獅子大王/src/components/bulbComponent.ts
@@ -0,0 +1,33 @@
+
+
+class bulbComponent extends eui.Component implements eui.UIComponent {
+
+	private lampGrad: egret.tween.TweenGroup;
+
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName: string, instance: any): void {
+		super.partAdded(partName, instance);
+	}
+
+
+	protected childrenCreated(): void {
+		super.childrenCreated();
+		this.playAnimation(this.lampGrad, true);
+	}
+
+	private playAnimation(target: egret.tween.TweenGroup, isLoop: boolean): void {
+		if (isLoop) {
+			for (var key in target.items) {
+				target.items[key].props = { loop: true };
+			}
+		}
+		target.play();
+	}
+
+}
+
+
+
