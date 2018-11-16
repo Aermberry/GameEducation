a7d9e80e0ea2cabecf2678efca95b4ec9e91868e
Salmonberry <sos2012happy@gmail.com>
2018-11-16 10:27:32 +0800

    AnimComponents

--- 26 獅子大王/src/scenes/rabbitScene.ts
+++ 26 獅子大王/src/scenes/rabbitScene.ts
@@ -1,12 +1,17 @@
 class rabbitScene extends eui.Component implements eui.UIComponent {
-
   private plantMask: egret.tween.TweenGroup;
   private tailWiggle: egret.tween.TweenGroup;
   private invitationCard: egret.tween.TweenGroup;
   private lionDialog: egret.tween.TweenGroup;
   private rabbitDialogBox: egret.tween.TweenGroup;
-	private bubleGrad: egret.tween.TweenGroup;
-	
+  private bubleGrad: egret.tween.TweenGroup;
+
+  private bulbGroup: eui.Group;
+  private achieveGroup: eui.Group;
+  private lionDialogGroup: eui.Group;
+  private rabbitDialogGroup: eui.Group;
+
+  private lion: eui.Image;
   public constructor() {
     super();
   }
@@ -17,5 +22,13 @@ class rabbitScene extends eui.Component implements eui.UIComponent {
 
   protected childrenCreated(): void {
     super.childrenCreated();
+    this.playAnim();
+  }
+
+  private async playAnim(): Promise<void> {
+    this.plantMask.play(0);
+    this.tailWiggle.play(0);
+    await lzlib.ThreadUtility.sleep(5000);
+    await this.invitationCard.play(0);
   }
 }
