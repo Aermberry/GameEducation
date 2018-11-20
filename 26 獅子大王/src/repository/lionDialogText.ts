eaa87c77f0b8f80e8f65ad0659d46164d3dc7b7d
Salmonberry <sos2012happy@gmail.com>
2018-11-20 12:02:24 +0800

    add res

--- 26 獅子大王/src/repository/lionDialogText.ts
+++ 26 獅子大王/src/repository/lionDialogText.ts
@@ -10,5 +10,18 @@ enum lionDialogText {
   rabbitText_c = '我要再邀請小動物參\n加嘉年華，快替我修\n改吧！',
   rabbitText_d = '做得好！\n放你一馬吧！',
   snakeText_a ='我請你來嘉年華，\n你爲什麽沒有來？',
-  snakeText_b ='噢！我忘了寫日期，你說得對！'
+  snakeText_b ='噢！我忘了寫日期，你說得對！',
+  pigText_a='我約定你「不見不\n散」，害我白等,\n真不識抬舉！',
+  pigText_a='噢！我忘了寫時\n間,你説的對！'
+  ratText_a='小老鼠，我連嘉年華\n舉行的地方都畫在卡上，你竟膽大不來！',
+  ratText_b='我已經在卡上畫上\n舉行地方的風景，',
+  ratText_c='那還不夠清楚？你\n説怎麽辦？',
+  sheepText_a='我請你來，你不\n來，真可惡！',
+  sheepText_b='你説得有理！你\n把我的印章蓋在適當的位置吧！',
+  sheepText_b='你説得有理！你\n把我的印章蓋在適當的位置吧！',
+  giraffeText_a='長頸鹿，既然收到\n邀請卡，請爲什麽不來？',
+  giraffeText_b='我要今天，即十一\n月三日，再發出邀\n請卡給小動物，',
+  giraffeText_c='應該怎麽寫？',
+  thanksTalk='幸好得到小動物們的提醒，\n我才知道邀請卡原來要這樣\n寫的！剛才我對牠們太凶了，讓我再修改卡上的用詞，希望小動物能感受到我是一個'
+  
 }
