18e44c8c181e3e0bed01ffd4da97762b93ee59a0
Salmonberry <sos2012happy@gmail.com>
2018-11-20 16:34:39 +0800

    invitation

---
+++ 26 獅子大王/src/scenes/resultScene.ts
@@ -0,0 +1,38 @@
+class resultScene extends eui.Component implements  eui.UIComponent {
+
+	private commitLabel:eui.Label;
+	private Anim:egret.tween.TweenGroup;
+
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+		this.textBook();
+		this.playAnimation(this.Anim,true);
+	}
+
+	private textBook():void {
+		this.commitLabel.text=lionDialogText.thanksTalk.toString();
+	}
+
+	private playAnimation(target: egret.tween.TweenGroup, isLoop: boolean): void {
+		if (isLoop) {
+			for (var key in target.items) {
+				target.items[key].props = { loop: true };
+			}
+		}
+		target.play();
+	}
+
+
+	
+}
