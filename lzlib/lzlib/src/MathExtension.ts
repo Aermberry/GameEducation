ebcfd77e7e92da61cc8a287a476dc400bc16e69b
Salmonberry <sos2012happy@gmail.com>
2018-11-22 15:27:38 +0800

    Update:lize

---
+++ lzlib/lzlib/src/MathExtension.ts
@@ -0,0 +1,9 @@
+// TypeScript file
+interface Math {
+    randomMinMax(min: number, max: number): number;
+}
+
+Math.randomMinMax = function (min: number, max: number): number
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
