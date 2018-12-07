e441e23e25995d320f1a3f5fd091239c485f51e9
wghuang <wghuang@liangzhicn.com>
2018-12-07 17:23:32 +0800

    #6号  加开始按钮修复

--- 6 减法概念/src/scenes/StartScene.ts
+++ 6 减法概念/src/scenes/StartScene.ts
@@ -1,47 +1,46 @@
-module StartScenes{
- export class StartScene extends eui.Component implements  eui.UIComponent {
-	private splashTweenGroup: egret.tween.TweenGroup;
-	private startButton2: eui.Image;
-	private startMask: eui.Rect;
+module StartScenes {
+	export class StartScene extends eui.Component implements eui.UIComponent {
+		private splashTweenGroup: egret.tween.TweenGroup;
+		private startButton2: eui.Image;
+		private startMask: eui.Rect;
 
-	private static instance: StartScene;
+		private static instance: StartScene;
 
-	public constructor() {
-		super();
-		StartScene.instance = this;
-	}
+		private startGameImage: eui.Image;
 
-	protected partAdded(partName:string,instance:any):void
-	{
-		super.partAdded(partName,instance);
-	}
+		public constructor() {
+			super();
+			StartScene.instance = this;
+		}
 
-
-	protected childrenCreated():void
-	{
-		super.childrenCreated();
-		mouse.enable(this.stage);
-		this.splashTweenGroup.addEventListener(egret.Event.COMPLETE, this.onSplashTweenGroupComplete, this);
-		this.startButton2.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStartButton2Click, this);
-		
-	}
-
-	public SplashTweenGroupComplete(): void {
-		Main.instance.gotoScene(new ChooseEquationScene());
-	}
+		protected partAdded(partName: string, instance: any): void {
+			super.partAdded(partName, instance);
+		}
 
 
-	private onStartButton2Click(): void
-	{
-		this.splashTweenGroup.play(0);
+		protected childrenCreated(): void {
+			super.childrenCreated();
+			mouse.enable(this.stage);
+			// this.splashTweenGroup.addEventListener(egret.Event.COMPLETE, this.onSplashTweenGroupComplete, this);
+			this.startButton2.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onStartButton2Click, this);
+			this.startGameImage.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onStartGameClick,this);
+
+		}
+
+		public SplashTweenGroupComplete(): void {
+			Main.instance.gotoScene(new ChooseEquationScene());
+		}
+
+		private async onStartButton2Click(): Promise <void> {
 		this.startMask.visible = false;
 		this.startButton2.visible = false;
+		await this.splashTweenGroup.playOnceAsync();
+		this.startGameImage.visible = true;
 		
 	}
 
-	private onSplashTweenGroupComplete(e: egret.Event): void
-	{
-		Main.instance.gotoScene(new ChooseEquationScene());
+		private onStartGameClick(e: egret.Event): void {
+			Main.instance.gotoScene(new ChooseEquationScene());
+		}
 	}
-}
 }
