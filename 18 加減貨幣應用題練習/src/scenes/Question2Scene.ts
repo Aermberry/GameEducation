46c45731cf360f30ac7ace9658c8105dfa66bce6
wghuang <wghuang@liangzhicn.com>
2018-12-30 13:21:36 +0800

    #18  bug修复

--- 18 加減貨幣應用題練習/src/scenes/Question2Scene.ts
+++ 18 加減貨幣應用題練習/src/scenes/Question2Scene.ts
@@ -91,7 +91,7 @@ class Question2Scene extends eui.Component implements eui.UIComponent {
 	// 下一步
 	private verification(): void {
 		const verification = StringUtility.trim(this.expression),
-			inputssion = "12元5角+41元2角",
+			inputssion = "12元5角+42元2角",
 			inputssiontwo = "42元2角+12元5角"
 		if (verification == inputssion || inputssiontwo) {
 			this.scarfImage.visible = false;
