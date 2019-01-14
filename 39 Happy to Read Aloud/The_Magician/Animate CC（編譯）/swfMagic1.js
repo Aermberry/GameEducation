e4b0314e5e468fea5d945b166c5a04ce4f90e769
Salmonberry <sos2012happy@gmail.com>
2019-01-14 14:49:57 +0800

    update

--- 39 Happy to Read Aloud/The_Magician/Animate CC（編譯）/swfMagic1.js
+++ 39 Happy to Read Aloud/The_Magician/Animate CC（編譯）/swfMagic1.js
@@ -1252,12 +1252,12 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"sounds/sound18.mp3", id:"sound18"},
-		{src:"sounds/sound19.mp3", id:"sound19"},
-		{src:"sounds/sound21.mp3", id:"sound21"},
-		{src:"sounds/sound23.mp3", id:"sound23"},
-		{src:"sounds/sound30.mp3", id:"sound30"},
-		{src:"sounds/sound6.mp3", id:"sound6"}
+		{src:"sounds/sound18.mp3?1547430796255", id:"sound18"},
+		{src:"sounds/sound19.mp3?1547430796255", id:"sound19"},
+		{src:"sounds/sound21.mp3?1547430796255", id:"sound21"},
+		{src:"sounds/sound23.mp3?1547430796255", id:"sound23"},
+		{src:"sounds/sound30.mp3?1547430796255", id:"sound30"},
+		{src:"sounds/sound6.mp3?1547430796255", id:"sound6"}
 	],
 	preloads: []
 };
