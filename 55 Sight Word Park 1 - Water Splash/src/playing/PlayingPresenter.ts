04926035422c22f03315a29c974cf0a566394d22
Salmonberry <sos2012happy@gmail.com>
2019-01-23 16:19:47 +0800

    fix(PlayingPresenter.ts & PlayingScene.ts): amend the Hint function & ReadLineLabel function
    this issue is produced because of the words array have complex relationship for the Hint function
    and the ReadLine Function;the Hint function need the global array ,the words to dynamic change the
    words.However,the ReadLine function need the origin array:world.SO,we have to save the origin
    arry:words to amend the functions.
    
    BREAKING CHANGE: hint function can diaplay normal
    
    fix ENG_IMG_0187a #1 #2 #3 #4

--- 55 Sight Word Park 1 - Water Splash/src/playing/PlayingPresenter.ts
+++ 55 Sight Word Park 1 - Water Splash/src/playing/PlayingPresenter.ts
@@ -3,62 +3,74 @@ class PlayingPresenter {
 	private puzzleBiz = new PuzzleBiz();
 	private view: IPlayingView;
 	private words: string[] = []; //puzzle中的单词表
+	private wordsCopy: string[] = [] //保存初始的words
 	private correctWordCount = 0; //答对的单词
 	private difficulty: Difficulty;
 
 	public constructor() {
 	}
 
-	public loadView(view: IPlayingView, difficulty: Difficulty): void
-	{
+	public loadView(view: IPlayingView, difficulty: Difficulty): void {
 		this.view = view;
 		this.difficulty = difficulty;
 		this.initGame();
 	}
 
-	private initGame(): void
-	{
+	private initGame(): void {
 		this.view.playReadyGoMovie();
 		this.nextPuzzleButtonClick();
+		this.wordsCopy = this.onCopyArry(this.words)
+		console.log("wordsCopy:" + this.wordsCopy)
 	}
 
-	public onShootButtonClick(): void
-	{
+	// 保存初始的數組
+	private onCopyArry(words: string[]): string[] {
+		let arry: string[] = [];
+		words.forEach(element => {
+			arry.push(element)
+		});
+		return arry
+	}
+
+	public onShootButtonClick(): void {
 		let selectedWord = this.view.currentSelectedChars.join('');
 		if (this.words.indexOf(selectedWord) >= 0) {
 			this.view.lockCurrentSelectChars();
-			this.view.markWordAsDeleted(this.words.indexOf(selectedWord));
+			console.log("當前的wordsCopy:" + this.wordsCopy)
+			console.log("wordsCopy中的index:" + this.wordsCopy.indexOf(selectedWord))
+			this.view.markWordAsDeleted(this.wordsCopy.indexOf(selectedWord));
 			console.log(selectedWord)
 			console.log(this.words)
-			this.handleWords(this.words,selectedWord);
+			this.handleWords(this.words, selectedWord);
 			console.log(this.words)
 			this.view.playGoodJobMovie();
 			this.correctWordCount++;
+			console.log("正確的個數：" + this.correctWordCount)
+			console.log("words的個數：" + this.words.length)
 		}
-
-		if (this.correctWordCount == this.words.length) {
+		// if (this.correctWordCount == this.words.length) {
+		// 	this.view.playCompleteAnimation();
+		// }
+		if (this.correctWordCount == 6) {
 			this.view.playCompleteAnimation();
 		}
 	}
 
-	private handleWords(words:string[],selectedWord:string):void{
-		let index=words.indexOf(selectedWord);
-		words.splice(index,1);
+	private handleWords(words: string[], selectedWord: string): void {
+		let index = words.indexOf(selectedWord);
+		words.splice(index, 1);
 	}
 
-	public onHintsBtnImgClick():void
-	{
-		console.log("num:"+this.correctWordCount);
+	public onHintsBtnImgClick(): void {
+		console.log("num:" + this.correctWordCount);
 		this.view.tipsanswer(0, this.words);
 	}
 
-	public onResetButtonClick(): void
-	{
+	public onResetButtonClick(): void {
 		this.view.deselectChars();
 	}
 
-	public nextPuzzleButtonClick(): void
-	{
+	public nextPuzzleButtonClick(): void {
 		this.correctWordCount = 0;
 		this.view.words = this.words = this.wordBiz.random(this.difficulty == Difficulty.easy ? 5 : 6);
 		this.view.puzzleCharacters = this.puzzleBiz.generate(this.words, this.difficulty == Difficulty.easy ? 5 : 8);
@@ -66,8 +78,7 @@ class PlayingPresenter {
 		this.view.markWordsAsNormal();
 	}
 
-	public onRestartButtonClick(): void
-	{
+	public onRestartButtonClick(): void {
 		this.initGame();
 	}
 }
