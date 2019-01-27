906f18ff43b3e54052921263fc595f2683d772b8
Salmonberry <sos2012happy@gmail.com>
2019-01-27 14:33:13 +0800

    docs:

--- 26 獅子大王/src/Main.ts
+++ 26 獅子大王/src/Main.ts
@@ -104,7 +104,7 @@ class Main extends eui.UILayer {
     protected createGameScene(): void {
         Main.instance = this;
         // this.addChild(new StartGmes.startScene());
-        this.addChild(new EditableLabel());
+        this.addChild(new StartGmes.startScene());
         // if (egret.getOption('scene')) {
         //     this.addChild(this.scenes[parseInt(egret.getOption('scene'))]); //允许用户跳转到指定场景
         // } else {
