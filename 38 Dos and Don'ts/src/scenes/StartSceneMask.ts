bdfaafc18e98cc885f20af29dc9d37ebbebedacc
Salmonberry <sos2012happy@gmail.com>
2019-03-07 10:58:38 +0800

    fix(StartSceneMask.ts):

--- 38 Dos and Don'ts/src/scenes/StartSceneMask.ts
+++ 38 Dos and Don'ts/src/scenes/StartSceneMask.ts
@@ -18,6 +18,7 @@ class StartSceneMask extends eui.Component implements  eui.UIComponent {
 	}
 
 	private async onStartButton(): Promise<void> {
+		//由於ios的限制，音頻的播放必須要在交互后才能播放（這個交互，必須帶有資源播放，才能帶動後面的資源的調用播放）
 		var SoundChannel:egret.SoundChannel= (RES.getRes("amy_go_shcool_mp3") as egret.Sound).play(0);
 		SoundChannel.volume=0;
 		this.playTweenGroup.play(0);
